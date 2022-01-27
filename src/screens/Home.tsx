1cf03b350ac4c80205a392377c5b849ba3ffbd45
Yahya <yahya@MacBook-Pro-4.local>
2022-01-27 16:41:47 +0100

    First Commit

---
+++ src/screens/Home.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Text, StyleSheet, SafeAreaView } from 'react-native';
+
+const Home = () => {
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.h1}>
+                Welcome
+            </Text>
+        </SafeAreaView>
+    );
+
+}
+
+export default Home;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'lightgrey',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    h1: {
+        color: '#22A25E',
+        fontSize: 25,
+        fontWeight: 'normal',
+        fontFamily: 'PublicSans-Thin_Light'
+    },
+    h2: {
+        color: '#22A25E',
+        fontSize: 25,
+        fontFamily: 'PublicSans-Thin_Light'
+    },
+});
